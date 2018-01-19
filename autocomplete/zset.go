55d6cd7f8f5384c888c6a15ddbed3584d5c15230
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2018-01-19 17:06:03 +0900

    Use pipeline

--- autocomplete/zset.go
+++ autocomplete/zset.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/go-redis/redis"
+	uuid "github.com/satori/go.uuid"
 )
 
 const validChars = "`abcdefghijklmnopqrstqvwxyz{"
@@ -15,15 +16,23 @@ func FindPrefixRange(prefix string) (start string, end string) {
 	return prefix[:len(prefix)-1] + suffix + "{", prefix + "{"
 }
 
-/*
 func AutocompleteOnPrefix(client *redis.Client, list string, prefix string) []string {
 	start, end := FindPrefixRange(prefix)
 	uv4 := uuid.Must(uuid.NewV4())
 	client.ZAdd(list, redis.Z{Score: 0, Member: start + uv4}, redis.Z{Score: 0, Member: end + uv4})
 	pipe := client.Pipeline()
 	sindex := pipeline.zrank(list, start)
+
+	err := client.Watch(func(tx *redis.Tx) error {
+		_, err = tx.Pipelined(func(pipe *redis.Pipeline) error {
+			sindex := pipe.ZRank(list, start)
+			eindex := pipe.ZRank(list, end)
+			pipe.Multi()
+			return nil
+		})
+		return err
+	}, list)
 }
-*/
 
 func JoinList(client *redis.Client, list string, user string) {
 	client.ZAdd(list, redis.Z{Score: 0, Member: user})
